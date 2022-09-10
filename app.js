ffad154f8a1389d23098d98203f57aaa7d11cd21
KyraCChurch <kchurch0114@gmail.com>
2022-09-10 12:48:48 -0600

    Made the start screen and start button works.

---
+++ app.js
@@ -0,0 +1,9 @@
+//Start Button
+const startBtn = document.querySelector('#start');
+const startPage = document.querySelector('.startPg');
+startBtn.addEventListener('click', (evt) => {
+    document.body.style.display = "none";
+    storyPt1.style.display = "block" 
+})
+//Story Start
+const storyPt1 = document.querySelector('.storyStart');
