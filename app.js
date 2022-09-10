9cfe7cf6d5729e1cc7e9e6db5ed5526fc6d23f26
KyraCChurch <kchurch0114@gmail.com>
2022-09-10 15:12:02 -0600

    Rock is now on screen and the first story line is displayed.

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 const startBtn = document.querySelector('#start');
 const startPage = document.querySelector('.startPg');
 startBtn.addEventListener('click', (evt) => {
-    document.body.style.display = "none";
-    storyPt1.style.display = "block" 
+    startPage.style.display = "none";
+//Transitioning to story start
+    storyPt1.style.display = "block";
+    document.body.style.background = "url('https://i.im.ge/2022/09/11/O4cgNy.210fb8977200036036103eceb02fa4ae.jpg') cover 0";
+    rock.style.display = 'block';
 })
 //Story Start
 const storyPt1 = document.querySelector('.storyStart');
+const nextBtn = document.querySelector('#next')
+
+
+//general
+const rock = document.querySelector('.rock')
