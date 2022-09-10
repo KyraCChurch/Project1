34d7347714161b09e3e42db9aefbde89da3003c0
KyraCChurch <kchurch0114@gmail.com>
2022-09-10 15:34:23 -0600

    Restart Button works

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //Start Button
 const startBtn = document.querySelector('#start');
 const startPage = document.querySelector('.startPg');
+
 startBtn.addEventListener('click', (evt) => {
     startPage.style.display = "none";
 //Transitioning to story start
@@ -8,10 +9,19 @@ startBtn.addEventListener('click', (evt) => {
     document.body.style.background = "url('https://i.im.ge/2022/09/11/O4cgNy.210fb8977200036036103eceb02fa4ae.jpg') cover 0";
     rock.style.display = 'block';
 })
+
 //Story Start
 const storyPt1 = document.querySelector('.storyStart');
 const nextBtn = document.querySelector('#next')
+nextBtn.addEventListener('click', (evt) => {
 
+    })
 
 //general
-const rock = document.querySelector('.rock')
+const rock = document.querySelector('.rock');
+const restart = document.querySelector('.reset');
+restart.addEventListener('click', (evt) => {
+    startPage.style.display = "block";
+    storyPt1.style.display = "none";
+    rock.style.display = "none";
+});
