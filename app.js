6c6ea5fb5e3865744cd1fabb3dc38ef71b33def5
KyraCChurch <kchurch0114@gmail.com>
2022-09-14 20:10:30 -0600

    next button works on story, started dialog

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ restart.addEventListener('click', (evt) => {
     firstStory.style.display = 'none';
     game1.style.display = 'none';
 });
-
+const deathScreen = document.querySelector('.death');
 //Start Button
 const startBtn = document.querySelector('#start');
 const startPage = document.querySelector('.startPg');
@@ -23,32 +23,51 @@ startBtn.addEventListener('click', (evt) => {
     storyPt1.style.display = "block";
     document.body.style.backgroundImage = `url(${backgroundImages[0]})`;
     rock.style.display = 'block';
-    firstStory.style.display = 'block';
 })
 
-//Story Start
+// Story Start
 const storyPt1 = document.querySelector('.storyStart');
-const nextBtn = document.querySelector('.next')
-const prevBtn = document.querySelector('.prev')
-const firstStory = document.querySelector('#firstPt')
-let currentStryIndex = 0
-let previousStryIndex = 0
-class storyTexts {
-    constructor(text){
-        this.text = text;
-    }
-}
-const story = [
-    new storyTexts("There once was a pet rock..."),
-    new storyTexts("He lived in the window and watched the days go by.")
-]
-nextBtn.addEventListener('click', (nextStory) => {
-    firstStory.style.display = 'none';
-    // const story1 = document.getElementsByClassName('.story');
-    // for (let i=0;i<story1.length;i+=3){
-    //   story1[i].style.display = 'block';
-    // }
+const nextBtn = document.querySelector('#next')
+const prevBtn = document.querySelector('#prev')
+let storyPosition = 0;
+const story = document.getElementsByClassName('story')
+const totalTexts = story.length;
+
+
+document.getElementById('next').addEventListener('click', function() {
+  moveToNextStory();
 })
+document.getElementById('prev').addEventListener('click', function() {
+  moveToPrevStory();
+});
+
+function updateStoryPos() {
+  for (let stories of story) {
+    stories.classList.remove('story--visible');
+    stories.classList.add('story--hidden');
+  }
+
+  story[storyPosition].classList.add('story--visible')
+}
+
+function moveToNextStory() {
+  updateStoryPos()
+
+  if (storyPosition === totalTexts - 1) {
+    storyPosition = 0;
+  } else {
+    storyPosition++;
+  }
+}
+function moveToPrevStory() {
+  updateStoryPos()
+
+  if (storyPosition === 0) {
+    storyPosition = 0;
+  } else {
+    storyPosition--;
+  }
+}
 
 // Start first game
 const gameBtn1 = document.querySelector('#gameBtn1')
@@ -62,52 +81,26 @@ gameBtn1.addEventListener('click', (evt) => {
     gameBtn1.style.display = "none";
     document.body.style.backgroundImage = `url(${backgroundImages[1]})`;
     document.body.style.backgroundColor = 'rgba(34, 30, 72, 1)';
-    fireflyAppear;
-});
+    setTimeout(() => {
+        fireflies.style.display = 'block';
+      }, Math.floor(Math.random() * (4000-1500) + 1500))
+    });
 // First game 
 const fireflies = document.querySelector('#firefly')
-// fireflies.addEventListener('click', (evt) => {
-// setTimeout(() => {
-//     fireflies.style.display = 'none';
-//   }, Math.floor(Math.random() * (4000-1500) + 1500))
-// });
-function fireflyAppear()
-    {
-    //   document.getElementById('firefly').style.display ='none'; //first hide the button
-      setTimeout(function(){ //using setTimeout function
-      document.getElementById('firefly').style.display ='block'; //displaying the button again after 3000ms or 3 seconds
+const multiples = document.querySelector('.fireflies')
+fireflies.addEventListener('click', (evt) => {
+    fireflies.style.display = 'none';
+    for (let i=0;i<multiples.length;i++){
+    setTimeout(function(){ 
+        document.getElementById('firefly').style.display ='block';
+    }, Math.floor(Math.random() * (4000-1000) + 1000));
+    setTimeout(() => {
+        fireflies.style.display = 'none';
+      }, 4500)
     }
-    ,3000); 
-    }
+})
+
     
 
 
 
-
-
-
-
-
-
-// const minTime = 1000;
-// const maxTime = 4000;
-// let msSinceClicked = 0;
-
-// let waiting = false;
-// fireflies.addEventListener("click", newPose)
-// function newPose() {
-//     //Makes button appear at random time between 1000 and 4000 ms.
-//     const appear = Math.floor(Math.random() * (maxTime - minTime + minTime));
-//     setTimeout(() => {
-//         fireflies.style.display = 'none';
-//         waiting = true;
-//     }, appear);
-// }
-// fireflies.addEventListener("click", () => {
-//     if (waiting) {
-//         const score = 1000 - msSinceClicked;
-//         waiting = false;
-//     }else {
-//         newPose();
-//     }
-// })
